e520523d19730ab72f550ef64f4b7e58bb73ab04
Putu Widyantara <putuwaw973@gmail.com>
2022-11-09 07:21:37 +0800

    Update local database connection using system environment

--- src/main/java/movieapp/util/DatabaseUtil.java
+++ src/main/java/movieapp/util/DatabaseUtil.java
@@ -12,7 +12,7 @@ import com.zaxxer.hikari.HikariDataSource;
  * @author Putu Widyantara
  */
 public class DatabaseUtil {
-     private static final HikariDataSource hikariDataSource;
+    private static final HikariDataSource hikariDataSource;
     static{
         HikariConfig configuration = new HikariConfig();
         configuration.setDriverClassName("com.mysql.cj.jdbc.Driver");
@@ -20,9 +20,9 @@ public class DatabaseUtil {
         // configuration.setPassword(System.getenv("DB_PASSWORD"));
         // configuration.setJdbcUrl("jdbc:mysql://" + System.getenv("DB_ADDRESS") + System.getenv("DB_NAME"));
 
-        configuration.setUsername("root");
-        configuration.setPassword("");
-        configuration.setJdbcUrl("jdbc:mysql://localhost:3306/db_movieapp");
+        configuration.setUsername(System.getenv("DB_USERNAME_LOCAL"));
+        configuration.setPassword(System.getenv("DB_PASSWORD_LOCAL"));
+        configuration.setJdbcUrl("jdbc:mysql://" + System.getenv("DB_ADDRESS_LOCAL") + System.getenv("DB_NAME_LOCAL"));
         hikariDataSource = new HikariDataSource(configuration);
     }
     public static HikariDataSource getDataSource(){
