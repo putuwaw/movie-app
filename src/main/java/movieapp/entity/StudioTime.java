31900c354cd83b801edce6a0eb8a0f8ae04fa3ed
Putu Widyantara <putuwaw973@gmail.com>
2022-11-12 20:26:08 +0800

    Create studio time entity

---
+++ src/main/java/movieapp/entity/StudioTime.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package movieapp.entity;
+
+/**
+ *
+ * @author Putu Widyantara
+ */
+public class StudioTime {
+    private String studioID;
+    private String time;
+    private String row;
+    private String column;
+    private String status;
+
+    public String getStudioID() {
+        return studioID;
+    }
+
+    public void setStudioID(String studioID) {
+        this.studioID = studioID;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public void setRow(String row) {
+        this.row = row;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
