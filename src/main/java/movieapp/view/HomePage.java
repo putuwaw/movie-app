2ab5bcd67ca589a314b0a4897784894ecd78b449
Putu Widyantara <putuwaw973@gmail.com>
2022-11-12 20:22:01 +0800

    Display playing film in homepage

--- src/main/java/movieapp/view/HomePage.java
+++ src/main/java/movieapp/view/HomePage.java
@@ -4,12 +4,19 @@
  */
 package movieapp.view;
 
+import movieapp.entity.Film;
+import movieapp.util.AppUtil;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JButton;
+
 /**
  *
  * @author Putu Widyantara
  */
 public class HomePage extends javax.swing.JFrame {
-
+    private Film[] playing;
+    
     /**
      * Creates new form HomePage
      */
@@ -27,6 +34,10 @@ public class HomePage extends javax.swing.JFrame {
     private void initComponents() {
 
         LoginButton = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -42,6 +53,34 @@ public class HomePage extends javax.swing.JFrame {
         });
         getContentPane().add(LoginButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 50, 70, 30));
 
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 110, 120, 190));
+
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 110, 120, 190));
+
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 110, 120, 190));
+
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 110, 120, 190));
+
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/home.jpg"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
@@ -53,14 +92,57 @@ public class HomePage extends javax.swing.JFrame {
         login.view();
         dispose();
     }//GEN-LAST:event_LoginButtonActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        MovieDetail movDetail = new MovieDetail();
+        movDetail.view(playing[0]);
+        dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        MovieDetail movDetail = new MovieDetail();
+        movDetail.view(playing[1]);
+        dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        MovieDetail movDetail = new MovieDetail();
+        movDetail.view(playing[2]);
+        dispose();
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        MovieDetail movDetail = new MovieDetail();
+        movDetail.view(playing[3]);
+        dispose();
+    }//GEN-LAST:event_jButton4ActionPerformed
+    
+    public void otherComponents(){
+        List<JButton> buttonList = new ArrayList<>();
+        buttonList.add(jButton1);
+        buttonList.add(jButton2);
+        buttonList.add(jButton3);
+        buttonList.add(jButton4);
+        playing = AppUtil.getFilmRepository().getPlayingMovie();
+        int i = 0;
+        for (JButton button: buttonList){
+            button.setIcon(new javax.swing.ImageIcon(getClass().getResource("/" + playing[i].getId()+ ".jpg")));
+            i++;
+        }
+    }
     
     public void view(){
+        otherComponents();
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton LoginButton;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
