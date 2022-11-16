3bdc6f5c05ca2c71ed4e251aa3cf8c0ec3440694
Putu Widyantara <putuwaw973@gmail.com>
2022-11-16 23:45:58 +0800

    Create snack entity

---
+++ src/main/java/movieapp/entity/Snack.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package movieapp.entity;
+
+/**
+ *
+ * @author Putu Widyantara
+ */
+public class Snack {
+    private String id;
+    private String name;
+    private String desc;
+    
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getDesc(){
+        return desc;
+    }
+    public void setDesc(String desc){
+        this.desc = desc;
+    }
+}
